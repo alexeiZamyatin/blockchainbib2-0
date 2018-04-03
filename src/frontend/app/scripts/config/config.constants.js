326a0f1172bd1d1874cdd3319661cac7740bd820
Alexei Zamyatin <azamyatin@sba-research.org>
2018-04-03 16:41:37 +0100

    redeployed in static folder

--- src/frontend/app/scripts/config/config.constants.js
+++ src/frontend/app/scripts/config/config.constants.js
@@ -3,8 +3,8 @@
 
   angular.module('frontendApp.config', [])
 
-    //.constant('BACKEND_API', 'http://128.131.169.35:8080/')
-    .constant('BACKEND_API', 'http://localhost:8080/')
+    .constant('BACKEND_API', 'http://http://146.169.47.91:8080/')
+    //.constant('BACKEND_API', 'http://localhost:8080/')
     .constant('VERSION_INFO', {version: '0.0.1', build: 'test build'})
 
     .constant('DEBUG', true)
