1a6742c0f8d75d7659064354043540102a5e41d0
Alexei Zamyatin <azamyatin@sba-research.org>
2018-04-03 15:21:41 +0100

    changed paths to mirror for more recent data

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 
-BIBTEXT_FILE = "blockchainbib/blockchain.bib"
+BIBTEXT_FILE = "blockchainbib_mirror/blockchain.bib"
 JSON_FILE= "data/blockchainbib.json"
-BIBTEX_HTML = "blockchainbib\blockchainbib_html\blockchain.html"
+BIBTEX_HTML = "blockchainbib_mirror\blockchainbib_html\blockchain.html"
